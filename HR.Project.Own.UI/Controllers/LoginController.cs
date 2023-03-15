a32c5394b3f8d9687711304902cd9c32a7da0613
akayslm <73195655+akayslm@users.noreply.github.com>
2023-03-16 00:00:21 +0300

    Login ve Register sayfalarında düzenlemeler yapıldı

--- HR.Project.Own.UI/Controllers/LoginController.cs
+++ HR.Project.Own.UI/Controllers/LoginController.cs
@@ -1,10 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HR.Project.Own.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HR.Project.Own.UI.Controllers
 {
     public class LoginController : Controller
     {
         public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            return View();
+        }
+        
+        public IActionResult Login()
         {
             return View();
         }
